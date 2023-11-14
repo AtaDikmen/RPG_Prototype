baa09777340fed8a2fd35a2f21e53e7817fd4f00
Ata Dikmen <ata.dikmen35@gmail.com>
2023-11-15 02:06:22 +0300

    A few updates

--- Assets/Scripts/Core/Health.cs
+++ Assets/Scripts/Core/Health.cs
@@ -1,5 +1,7 @@
+using RPG.Combat;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,10 +10,37 @@ namespace RPG.Core
     public class Health : MonoBehaviour
     {
         public float health = 100f;
+        [SerializeField] ParticleSystem _particleSystem;
         [SerializeField] UnityEvent onDamage;
+        [SerializeField] UnityEvent onHealing;
         [SerializeField] UnityEvent onDie;
 
         bool isDead = false;
+
+        void Update()
+        {
+            if (_particleSystem != null)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    StartCoroutine(HealthPotion());
+                }
+            }
+            
+        }
+
+        private IEnumerator HealthPotion()
+        {
+            onHealing.Invoke();
+
+            for (int i = 0; i < 4; i++)
+            {
+                health += 10f;
+                _particleSystem.Play();
+                yield return new WaitForSeconds(0.5f);
+                
+            }
+        }
 
         public bool IsDead()
         {
